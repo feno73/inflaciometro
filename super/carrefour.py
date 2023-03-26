b189d27ea4853d9cbd846a40e11f5f1182eb196f
Feno <diego.el.feno@gmail.com>
2023-03-26 14:49:41 -0300

    Optimización del código

--- super/carrefour.py
+++ super/carrefour.py
@@ -5,34 +5,41 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
 
-driver.get("https://www.carrefour.com.ar/Almacen/Aceites-y-vinagres")
-time.sleep(10)
-driver.execute_script("window.scrollTo(0,300);")
-time.sleep(10)
+with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options) as driver:
+    driver.get("https://www.carrefour.com.ar/Almacen/Aceites-y-vinagres")
+    time.sleep(10)
+    driver.execute_script("window.scrollTo(0,300);")
+    time.sleep(10)
+    body = driver.execute_script("return document.body")
+    source = body.get_attribute('innerHTML')
+    soup = BeautifulSoup(source, "lxml")
+    productos = soup.select("div.lyracons-product-summary-status-0-x-container")
 
-body = driver.execute_script("return document.body")
-source = body.get_attribute('innerHTML')
+    lista_productos = []
+    for item in productos:
+        nombre = item.select_one(
+            "span.vtex-product-summary-2-x-productBrand.vtex-product-summary-2-x-brandName.t-body").text
+        precio_entero = item.select_one("span.lyracons-carrefourarg-product-price-1-x-currencyInteger").text
+        precio_decimal = item.select_one("span.lyracons-carrefourarg-product-price-1-x-currencyFraction").text
+        ##Por ahora hardcodeo esta parte:
+        marca, created = Marca.get_or_create(nombre="N/A")
+        categoria, created = Categoria.get_or_create(nombre="Aceites y vinagres")
+        producto = Producto(
+            nombre=nombre,
+            supermercado="Carrefour",
+            marca=marca,
+            categoria=categoria,
+            precio=float(f"{precio_entero}.{precio_decimal}"),
+            canasta_basica=True
+        )
+        lista_productos.append(producto)
 
-soup = BeautifulSoup(source, "lxml")
-productos = soup.find_all("div", class_="lyracons-product-summary-status-0-x-container")
+    Producto.bulk_create(lista_productos)
 
-for item in productos:
-    nombre = item.find("span",
-                       class_="vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body").text
-    precio_entero = item.find("span", class_="lyracons-carrefourarg-product-price-1-x-currencyInteger").text
-    precio_decimal = item.find("span", class_="lyracons-carrefourarg-product-price-1-x-currencyFraction").text
-    marca, created = Marca.get_or_create(nombre="N/A")
-    categoria, created = Categoria.get_or_create(nombre="Aceites y vinagres")
-    # print(f"{nombre} | $ {precio_entero},{precio_decimal}")
-    producto = Producto.create(
-        nombre=nombre,
-        supermercado="Carrefour",
-        marca=marca,
-        categoria=categoria,
-        precio=float(f"{precio_entero}.{precio_decimal}"),
-        canasta_basica=True
-    )
 
 ##TO-DO: Iterar la paginacion mientras el boton ">" exista
+##Podria armar aparte la funcion que recorre el html y saca los productos
+##y dejar la logica de paginado
