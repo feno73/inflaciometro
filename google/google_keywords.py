b58ccd5a170beb1a6648b2e70d34ea9f55ad4ed3
feno73 <diego.el.feno@gmail.com>
2023-08-27 19:26:14 -0300

    guarda los links de los resultado

--- google/google_keywords.py
+++ google/google_keywords.py
@@ -1,41 +1,35 @@
 import time
-
 from selenium.webdriver.common.by import By
-
 from models import Keyword
 from utils.log import log_debug
 from utils.selenium_conf import get_driver
+from bs4 import BeautifulSoup
 
 
-def obtener_resultados(driver, url):
+def obtener_resultados(driver, url, links):
     driver.get(url)
     time.sleep(5)
-    resultados = driver.find_element(By.ID, 'rso')
-    divs = resultados.find_elements(By.TAG_NAME, 'div')
-    for div in divs:
-        #print(div.text)
-        log_debug(f"Aca va una etiqueta a ------------------------------------------------OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-        a = div.find_element(By.XPATH, '//div/div/div[1]/div/div/a')
-        url = a.get_attribute("href")
-        print(a)
-    #for resultado in resultados:
-    #print(resultados.get_attribute("outerHTML"))
-
-    return resultados
+    resultados = BeautifulSoup(driver.page_source, 'html.parser')
+    search = resultados.find_all('div', class_="yuRUbf")
+    for h in search:
+        links.append(h.a.get('href'))
 
 
 def googlescrapp():
     keywords = Keyword.select()
+    links = []
 
     with get_driver() as driver:
         for keyword in keywords:
             log_debug(f"Buscando la palabra: {keyword.nombre}")
+            n_pages = 2
+            for page in range(1, n_pages):
+                ## Ejemplo "https://www.google.com/search?q=selenium+grid+LiveView+(VNC)+Password"
+                url = f"https://www.google.com/search?q={keyword.nombre}&start={(page - 1) * 10}"
+                log_debug(f"Accediendo a {url}")
+                resultados = obtener_resultados(driver, url, links)
 
-            ## Ejemplo "https://www.google.com/search?q=selenium+grid+LiveView+(VNC)+Password"
-            url = f"https://www.google.com/search?q={keyword.nombre}"
-            log_debug(f"Accediendo a {url}")
-            resultados = obtener_resultados(driver, url)
-            #print(resultados)
+    print(links)
 
 
 if __name__ == '__main__':
@@ -46,6 +40,13 @@ if __name__ == '__main__':
 
 
 
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    # soup = BeautifulSoup(r.text, 'html.parser')
+
+    search = soup.find_all('div', class_="yuRUbf")
+    for h in search:
+        links.append(h.a.get('href'))
 
 
 
